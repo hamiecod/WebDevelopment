478ea565323f7fd0d6b42142da6fa1d97d6ec44e
hamiecod <hargunbeersingh@gmail.com>
2021-02-21 09:23:34 +0530

    learnt about npm

---
+++ tut69/tut69.js
@@ -0,0 +1,26 @@
+console.log("This is tutorial 69.0.0");
+// versions
+// 1.0.0 is hte first version
+// major.minor.patch
+// the patches are fixes for already existing functions
+// the minors are the addition of functions which do not fiddle with the existing functions
+// the majors are the changes in the existing functions
+
+// save dev install
+// there are some packages which you only need during the time of development and do not require them in production or the finished product
+// in this case you install them in save-dev mode
+// you install the packages in save dev mode as follows:
+// install nodemom --save-dev
+// this makes it deve dependency
+// the package will be removed when the app is published
+// nodemon is just like live server and automatically restarts the server at a specific interval
+
+// if you want to install a particular package globally in your computer
+// prefix the install command with '--global'
+
+// you can run nodemon by replacing node with it
+
+// the version are prefixed by '^','~' and '>'
+// '^' installs the exact versin
+// '~' installs the exact major and minor and installs the latest patches
+// '>' installs the latest version available
